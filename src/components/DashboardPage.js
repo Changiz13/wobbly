527295c19b1ebd57a3ed70bd8cea92a8de9f8651
Changiz Hosseini <zyddal1310@gmail.com>
2018-03-23 16:27:24 +0100

    routed up redux on dashboard page

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import AddTodo from './AddTodo';
-import TodoList from './TodoList';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import TopSection from './TopSection';
+import * as TodoActions from '../actions/todos';
 
-const DashboardPage = () => (
+const DashboardPage = ({ todos, actions }) => (
   <div>
-    <AddTodo />
-    <TodoList />
+    <TopSection addTodo={actions.addTodo} />
   </div>
 );
 
-export default DashboardPage;
+const mapStateToProps = (state) => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(TodoActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardPage);
