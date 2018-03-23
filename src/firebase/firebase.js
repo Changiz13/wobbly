2f8918930894d8012a3440c0f3873c0c7323217c
Changiz Hosseini <zyddal1310@gmail.com>
2018-03-23 21:08:45 +0100

    changed firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,18 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyCr8GmJUq-7muHhN8cBOp-tqTjQ3ZDi9BI",
+  authDomain: "wobbly-8f04f.firebaseapp.com",
+  databaseURL: "https://wobbly-8f04f.firebaseio.com",
+  projectId: "wobbly-8f04f",
+  storageBucket: "",
+  messagingSenderId: "787468132464"
+/*   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID */
 };
 
 firebase.initializeApp(config);
